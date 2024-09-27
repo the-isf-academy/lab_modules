1f4c108639df2b1c2436f4942515776d1b7b7948
emma <eqbrown@isf.edu.hk>
2024-09-27 11:09:49 +0800

    #test let's see how this work #feedback question #next_class try changing colors

--- tree_drawing/tree.py
+++ tree_drawing/tree.py
@@ -1,17 +1,22 @@
 # tree.py
 
 from turtle import *
-# 💻 IMPORT MODULES BELOW  💻 #
+from tree_parts import tree_trunk, tree_top 
+
+
+def tree_full(size):
+    tree_trunk(size)
+    backward(size/4)
+    tree_top(size)
+
+
+tree_full(100)
+
+input()
 
 
 
 
-# 💻 WRITE THE tree_full() FUNCTION BELOW  💻 #
 
-
-
-
-
-# 💻 DON'T FORGET TO CALL THE FUNCTION BELOW  💻 #
 
 
