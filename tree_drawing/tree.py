b469060b9dff89ac9809f0f3a01d43c015304e95
yjhan_isf <yjhan@isf.edu.hk>
2025-09-18 13:06:05 +0800

    solutions init

--- tree_drawing/tree.py
+++ tree_drawing/tree.py
@@ -1,22 +1,32 @@
 # tree.py
 
 from turtle import *
+from random import randint
 
 # 💻 IMPORT MODULES BELOW  💻 #
-
-
+from tree_parts import tree_trunk, tree_top 
 
 # 💻 WRITE THE tree_full() FUNCTION BELOW  💻 #
-    
+def tree_full(size):
+    tree_trunk(size)
+    backward(size/4)
+    tree_top(size)
+
+def forest():
+    for i in range (5):
+        penup()
+        x_coordinate = randint(-300,300) # generates a random integer between -200 and 200
+        y_coordinate = randint(-200,200)
+        goto(x_coordinate, y_coordinate)
+        pendown()
+        if (y_coordinate > 0): # if the tree is far
+            tree_full(randint(50,100))
+        else:
+            tree_full(randint(100,200))
 
 # 💻 DON'T FORGET TO CALL THE FUNCTION BELOW  💻 #
-
+# tree_full(100)
+speed(0)
+forest()
 
 input()
-
-
-
-
-
-
-
